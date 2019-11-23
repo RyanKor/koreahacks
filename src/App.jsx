ebd5725ac214b3fcad64eb8c4bf5f0e43f83b800
김세진 <sejjj120@gmail.com>
2019-11-23 18:49:49 +0900

    refactor: change file extentions

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
+
+export default App;
