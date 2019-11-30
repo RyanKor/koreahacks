fbe77bbe5074f5ad7e66ed3cc8315203c92bc50e
김세진 <sejjj120@gmail.com>
2019-12-01 01:55:20 +0900

    feat: 반응형 css 1024px 기준
    1023px 까지는 햄버거 버튼과 메인 컴포넌트 padding 0.
    1024부터 메인 컴포넌트 padding 존재.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import "./App.css";
 
 const App = () => (
   <div className="App">
